d27a1d2e1758be1aa8f3736ab76575ef244c2b45
Forrest Loomis <cybercyst@gmail.com>
2024-01-29 08:40:00 -0500

    comment out WIP xml encoding

--- aws-mock/main.go
+++ aws-mock/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/xml"
+	"io"
 	"net/http"
 	"time"
 
@@ -33,16 +33,16 @@ var (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data, err := xml.Marshal(MockListBucketsResponse)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte("Internal Server Error"))
-		}
-
-		_, _ = w.Write(data)
-		// 		_, _ = io.WriteString(w, `<?xml version='1.0' encoding='utf-8'?>
-		// <ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Owner><DisplayName>webfile</DisplayName><ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID></Owner><Buckets><Bucket><Name>test</Name><CreationDate>2024-01-29T11:33:51.000Z</CreationDate></Bucket></Buckets></ListAllMyBucketsResult>
-		// `)
+		// data, err := xml.Marshal(MockListBucketsResponse)
+		// if err != nil {
+		// 	w.WriteHeader(http.StatusInternalServerError)
+		// 	_, _ = w.Write([]byte("Internal Server Error"))
+		// }
+		//
+		// _, _ = w.Write(data)
+		_, _ = io.WriteString(w, `<?xml version='1.0' encoding='utf-8'?>
+		<ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Owner><DisplayName>webfile</DisplayName><ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID></Owner><Buckets><Bucket><Name>test</Name><CreationDate>2024-01-29T11:33:51.000Z</CreationDate></Bucket></Buckets></ListAllMyBucketsResult>
+		`)
 	})
 
 	err := http.ListenAndServe(":4566", nil)
