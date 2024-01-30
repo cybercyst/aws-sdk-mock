af71dac7874d7ec195b133664440bdbae089afbb
Forrest Loomis <cybercyst@gmail.com>
2024-01-29 22:43:30 -0500

    add simple switch between s3 and ec2

--- aws-mock/main.go
+++ aws-mock/main.go
@@ -3,46 +3,22 @@ package main
 import (
 	"io"
 	"net/http"
-	"time"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
-)
-
-type ListAllMyBucketsResult struct {
-	s3.ListBucketsOutput
-}
-
-var (
-	MockListBucketsResponse = ListAllMyBucketsResult{
-		ListBucketsOutput: s3.ListBucketsOutput{
-			Buckets: []types.Bucket{
-				{
-					CreationDate: aws.Time(time.Now()),
-					Name:         aws.String("mock-bucket"),
-				},
-			},
-			Owner: &types.Owner{
-				ID:          aws.String("AIDACKCEVSQ6C2EXAMPLE"),
-				DisplayName: aws.String("Account+Name"),
-			},
-		},
-	}
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// data, err := xml.Marshal(MockListBucketsResponse)
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	_, _ = w.Write([]byte("Internal Server Error"))
-		// }
-		//
-		// _, _ = w.Write(data)
-		_, _ = io.WriteString(w, `<?xml version='1.0' encoding='utf-8'?>
-		<ListAllMyBucketsResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Owner><DisplayName>webfile</DisplayName><ID>75aa57f09aa0c8caeab4f8c24e99d10f8e7faeebf76c078efc7c6caea54ba06a</ID></Owner><Buckets><Bucket><Name>test</Name><CreationDate>2024-01-29T11:33:51.000Z</CreationDate></Bucket></Buckets></ListAllMyBucketsResult>
-		`)
+		// localstack has a lot of logic to detect which service to use
+		// but i believe for our services we can parse authorization
+		// content= for the service, which is the 4th chunk
+		authorization := r.Header.Get("authorization")
+
+		switch {
+		case strings.Contains(authorization, "s3"):
+			_, _ = io.WriteString(w, MockListBuckets)
+		case strings.Contains(authorization, "ec2"):
+			_, _ = io.WriteString(w, MockDescribeInstances)
+		}
 	})
 
 	err := http.ListenAndServe(":4566", nil)
